5b1737727f495c190cff27ec21382bcc1b46f699
Mariano Barcia <mariano.barcia@gmail.com>
2016-03-08 18:28:12 +0100

    DRUPSIBLE-52 HOME for tilde.
    Signed-off-by: Mariano Barcia <mariano.barcia@gmail.com>

--- bin/ssh-agent.sh
+++ bin/ssh-agent.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-SSH_AGENT_DATA="~/.ssh-agent"
+SSH_AGENT_DATA="$HOME/.ssh-agent"
 ID_FILE="${1}"
 if [ "${ID_FILE}" == "" ]; then
-	ID_FILE="~/.ssh/id_rsa"
+	ID_FILE="$HOME/.ssh/id_rsa"
 fi
 
 ssh-add -l &>/dev/null
 if [ "$?" == 2 ]; then
-	test -r $SSH_AGENT_DATA && eval "$(<${SSH_AGENT_DATA/#\~/$HOME})" >/dev/null
+	test -r "$SSH_AGENT_DATA" && eval "$(<${SSH_AGENT_DATA/#\~/$HOME})" >/dev/null
 	ssh-add -l &>/dev/null
 	if [ "$?" == 2 ]; then
 		(umask 066; ssh-agent > "${SSH_AGENT_DATA/#\~/$HOME}")
