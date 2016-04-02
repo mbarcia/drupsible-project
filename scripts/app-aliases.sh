0c6f404b19d16c0e92e2fb3d8a76bdca5e0db265
Mariano Barcia <mariano.barcia@gmail.com>
2016-04-02 13:06:36 +0200

    Changed white space/line endings.
    Signed-off-by: Mariano Barcia <mariano.barcia@gmail.com>

--- scripts/app-aliases.sh
+++ scripts/app-aliases.sh
@@ -1,12 +1,10 @@
 #
 # Section added by Drupsible
 #
-
 # drupsible aliases
 alias pitiribi-deploy='ansible-playbook -i ansible/inventory/pitiribi-local ansible/playbooks/deploy.yml -e "app_name=pitiribi app_target=local"'
 alias pitiribi-config='ansible-playbook -i ansible/inventory/pitiribi-local ansible/playbooks/config.yml -e "app_name=pitiribi app_target=local"'
 alias pitiribi-config-deploy='ansible-playbook -i ansible/inventory/pitiribi-local ansible/playbooks/config-deploy.yml -e "app_name=pitiribi app_target=local"'
-
 #
 # End of section by Drupsible
 #
