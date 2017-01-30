82915a024c95af664f11ab769ade48f135f5a9a6
Mariano Barcia <mariano.barcia@gmail.com>
2017-01-30 01:45:22 +0000

    DRUPSIBLE-248 Changed paths to /vagrant instead of relative to home dir.

--- scripts/app-aliases.sh
+++ scripts/app-aliases.sh
@@ -2,9 +2,9 @@
 # Section added by Drupsible
 #
 # drupsible aliases
-alias pitiribi-deploy='ansible-playbook -i ansible/inventory/pitiribi-local ansible/playbooks/deploy.yml -e "app_name=pitiribi app_target=local"'
-alias pitiribi-config='ansible-playbook -i ansible/inventory/pitiribi-local ansible/playbooks/config.yml -e "app_name=pitiribi app_target=local"'
-alias pitiribi-config-deploy='ansible-playbook -i ansible/inventory/pitiribi-local ansible/playbooks/config-deploy.yml -e "app_name=pitiribi app_target=local"'
+alias pitiribi-deploy='ansible-playbook -i /vagrant/ansible/inventory/pitiribi-local /vagrant/ansible/playbooks/deploy.yml -e "app_name=pitiribi app_target=local"'
+alias pitiribi-config='ansible-playbook -i /vagrant/ansible/inventory/pitiribi-local /vagrant/ansible/playbooks/config.yml -e "app_name=pitiribi app_target=local"'
+alias pitiribi-config-deploy='ansible-playbook -i /vagrant/ansible/inventory/pitiribi-local /vagrant/ansible/playbooks/config-deploy.yml -e "app_name=pitiribi app_target=local"'
 #
 # End of section by Drupsible
 #
